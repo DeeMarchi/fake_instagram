65fc0fad9d4bfb7026318bb7fd9f910102b5f6a8
Denner Marchi <denner883@hotmail.com>
2020-04-21 16:37:28 -0300

    Preparando a criptografia da senha ao cadastrar

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+
 const IndexController = {
 
     login: (req, res) => {
@@ -14,6 +16,11 @@ const IndexController = {
 
     registrar: (req, res) => {
         console.log(req.body);
+
+        const { email, nomeCompleto, usuario, senha } = req.body;
+        const senhaCrypto = bcrypt.hashSync(senha, 10);
+
+        console.log(senhaCrypto);
     },
 
     home: (req, res) => {
